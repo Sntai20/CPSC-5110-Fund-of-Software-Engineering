55b46e1cd49d4c588a7806228093cfe3f3f0cde5
Antonio Santana <ansantan@outlook.com>
2022-10-11 13:23:26 -0700

    Test commit.

--- src/Startup.cs
+++ src/Startup.cs
@@ -37,11 +37,13 @@ namespace ContosoCrafts.WebSite
             else
             {
                 app.UseExceptionHandler("/Error");
+
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
+
             app.UseStaticFiles();
 
             app.UseRouting();
