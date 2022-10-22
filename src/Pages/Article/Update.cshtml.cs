0cf9f3a8860d01f2ab012050373e0f80ab7a51a3
AndrewTheLowly <cshell@seattleu.edu>
2022-10-22 07:55:19 -0700

    Created Update.cshtml/Update.cshmtl.cs in Articles folder.

---
+++ src/Pages/Article/Update.cshtml.cs
@@ -0,0 +1,62 @@
+using System.Linq;
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+
+namespace ContosoCrafts.WebSite.Pages.Article
+{
+    /// <summary>
+    /// Manage the Update of the data for a single record
+    /// </summary>
+    public class UpdateModel : PageModel
+    {
+        // Data middletier
+        public JsonFileArticleService ProductService { get; }
+
+        /// <summary>
+        /// Defualt Construtor
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="productService"></param>
+        public UpdateModel(JsonFileArticleService productService)
+        {
+            ProductService = productService;
+        }
+
+        // The data to show, bind to it for the post
+        [BindProperty]
+        public ArticleModel Article { get; set; }
+
+        /// <summary>
+        /// REST Get request
+        /// Loads the Data
+        /// </summary>
+        /// <param name="id"></param>
+        public void OnGet(string id)
+        {
+            Article = ProductService.GetAllData().FirstOrDefault(m => m.Id.Equals(id));
+        }
+
+        /// <summary>
+        /// Post the model back to the page
+        /// The model is in the class variable Product
+        /// Call the data layer to Update that data
+        /// Then return to the index page
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            ProductService.UpdateData(Article);
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
