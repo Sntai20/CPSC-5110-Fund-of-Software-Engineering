a45ce064cdd0b05f8a9c0d8a71318ccaed4bf9d6
AndrewTheLowly <cshell@seattleu.edu>
2022-10-28 19:29:30 -0700

    Created unit test for OnGet method in Read.cshtml.Tests.cs

--- UnitTests/Pages/Article/Read.cshtml.Tests.cs
+++ UnitTests/Pages/Article/Read.cshtml.Tests.cs
@@ -1,11 +1,90 @@
-﻿using System;
-namespace UnitTests.Pages.Article
+﻿using System.Linq;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages.Article;
+using ContosoCrafts.WebSite.Services;
+
+namespace UnitTests.Pages.Article.Read
 {
-    public class Read
+    public class ReadTests
     {
-        public Read()
+        #region TestSetup
+        public static IUrlHelperFactory urlHelperFactory;
+        public static DefaultHttpContext httpContextDefault;
+        public static IWebHostEnvironment webHostEnvironment;
+        public static ModelStateDictionary modelState;
+        public static ActionContext actionContext;
+        public static EmptyModelMetadataProvider modelMetadataProvider;
+        public static ViewDataDictionary viewData;
+        public static TempDataDictionary tempData;
+        public static PageContext pageContext;
+
+        public static ReadModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
         {
+            httpContextDefault = new DefaultHttpContext()
+            {
+                //RequestServices = serviceProviderMock.Object,
+            };
+
+            modelState = new ModelStateDictionary();
+
+            actionContext = new ActionContext(httpContextDefault, httpContextDefault.GetRouteData(), new PageActionDescriptor(), modelState);
+
+            modelMetadataProvider = new EmptyModelMetadataProvider();
+            viewData = new ViewDataDictionary(modelMetadataProvider, modelState);
+            tempData = new TempDataDictionary(httpContextDefault, Mock.Of<ITempDataProvider>());
+
+            pageContext = new PageContext(actionContext)
+            {
+                ViewData = viewData,
+            };
+
+            var mockWebHostEnvironment = new Mock<IWebHostEnvironment>();
+            mockWebHostEnvironment.Setup(m => m.EnvironmentName).Returns("Hosting:UnitTestEnvironment");
+            mockWebHostEnvironment.Setup(m => m.WebRootPath).Returns("../../../../src/bin/Debug/net6.0/wwwroot");
+            mockWebHostEnvironment.Setup(m => m.ContentRootPath).Returns("./data/");
+
+            var MockLoggerDirect = Mock.Of<ILogger<IndexModel>>();
+            JsonFileArticleService articleService;
+
+            articleService = new JsonFileArticleService(mockWebHostEnvironment.Object);
+
+            pageModel = new ReadModel(articleService)
+            {
+            };
         }
+
+        #endregion TestSetup
+
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Should_Return_Articles()
+        {
+            // Arrange
+            var data = TestHelper.ArticleService.GetAllData().First();
+
+            // Act
+            pageModel.OnGet(data.Id);
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+        }
+        #endregion OnGet
     }
 }
-
