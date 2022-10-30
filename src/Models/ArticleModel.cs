b310217aa1e8e8c07019aa62170c7f20b9cda982
Antonio Santana <ansantan@microsoft.com>
2022-10-29 17:14:25 -0700

    Applied codestyle convention to ArticleModel.cs file.

--- src/Models/ArticleModel.cs
+++ src/Models/ArticleModel.cs
@@ -1,7 +1,7 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.ComponentModel.DataAnnotations;
-using System.Collections.Generic;
 
 namespace ContosoCrafts.WebSite.Models
 {
@@ -16,8 +16,8 @@ namespace ContosoCrafts.WebSite.Models
         public string Image { get; set; }
 
         public string Url { get; set; }
-        
-        [StringLength (maximumLength: 33, MinimumLength = 1, ErrorMessage = "The Title should have a length of more than {2} and less than {1}")]
+
+        [StringLength(maximumLength: 33, MinimumLength = 1, ErrorMessage = "The Title should have a length of more than {2} and less than {1}")]
         public string Title { get; set; }
 
         public string Description { get; set; }
@@ -26,7 +26,7 @@ namespace ContosoCrafts.WebSite.Models
 
         public ArticleTypeEnum ArticleType { get; set; } = ArticleTypeEnum.Undefined;
 
-        [Range (-1, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        [Range(-1, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
         public int Price { get; set; }
 
         // Store the Comments entered by the users on this article.
