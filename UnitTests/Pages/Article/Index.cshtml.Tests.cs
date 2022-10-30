7aabd1539b295b72d32ca307638c9bba3467dc18
Antonio Santana <ansantan@microsoft.com>
2022-10-29 18:08:42 -0700

    Applied codestyle convention to the Index.cshtml.Tests.cs file.

--- UnitTests/Pages/Article/Index.cshtml.Tests.cs
+++ UnitTests/Pages/Article/Index.cshtml.Tests.cs
@@ -1,22 +1,22 @@
-﻿using ContosoCrafts.WebSite.Pages.Article;
-using ContosoCrafts.WebSite.Services;
-
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Routing;
-using Microsoft.AspNetCore.Mvc.ViewFeatures;
-using Microsoft.AspNetCore.Routing;
-using Microsoft.Extensions.Logging;
-
-using Moq;
-
-using NUnit.Framework;
-
-namespace UnitTests.Pages.Article.Index
+﻿namespace UnitTests.Pages.Article
 {
+    using ContosoCrafts.WebSite.Pages.Article;
+    using ContosoCrafts.WebSite.Services;
+
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.ModelBinding;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.AspNetCore.Mvc.Routing;
+    using Microsoft.AspNetCore.Mvc.ViewFeatures;
+    using Microsoft.AspNetCore.Routing;
+    using Microsoft.Extensions.Logging;
+
+    using Moq;
+
+    using NUnit.Framework;
+
     public class IndexTests
     {
         #region TestSetup
@@ -54,9 +54,9 @@ namespace UnitTests.Pages.Article.Index
             };
 
             var mockWebHostEnvironment = new Mock<IWebHostEnvironment>();
-            mockWebHostEnvironment.Setup(m => m.EnvironmentName).Returns("Hosting:UnitTestEnvironment");
-            mockWebHostEnvironment.Setup(m => m.WebRootPath).Returns("../../../../src/bin/Debug/net6.0/wwwroot");
-            mockWebHostEnvironment.Setup(m => m.ContentRootPath).Returns("./data/");
+            _ = mockWebHostEnvironment.Setup(m => m.EnvironmentName).Returns("Hosting:UnitTestEnvironment");
+            _ = mockWebHostEnvironment.Setup(m => m.WebRootPath).Returns("../../../../src/bin/Debug/net6.0/wwwroot");
+            _ = mockWebHostEnvironment.Setup(m => m.ContentRootPath).Returns("./data/");
 
             var MockLoggerDirect = Mock.Of<ILogger<IndexModel>>();
             JsonFileArticleService articleService;
@@ -75,7 +75,7 @@ namespace UnitTests.Pages.Article.Index
         public void OnGet_Valid_Should_Return_Articles()
         {
             // Arrange
-            var data = pageModel.Articles;
+            _ = pageModel.Articles;
 
             // Act
             pageModel.OnGet();
