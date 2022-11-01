536f5d3aed711538088e1fcdee481f0eda717fa4
Antonio Santana <ansantan@microsoft.com>
2022-11-01 13:02:42 -0700

    Added normal documentation with the summary, param values, and the return value to the JsonFileArticleService.cs file.

--- src/Services/JsonFileArticleService.cs
+++ src/Services/JsonFileArticleService.cs
@@ -1,4 +1,4 @@
-namespace ContosoCrafts.WebSite.Services
+﻿namespace ContosoCrafts.WebSite.Services
 {
     using System.Collections.Generic;
     using System.IO;
@@ -9,17 +9,35 @@ namespace ContosoCrafts.WebSite.Services
 
     using Microsoft.AspNetCore.Hosting;
 
+    /// <summary>
+    /// The JsonFileArticleService class is responsible for interacting with the
+    /// datastore. The datastore for this project is the articles.json file.
+    /// </summary>
     public class JsonFileArticleService
     {
+        /// <summary>
+        /// This is the default constructor.
+        /// </summary>
+        /// <param name="webHostEnvironment">Provides the information about the web hosting environment an application is running in.</param>
         public JsonFileArticleService(IWebHostEnvironment webHostEnvironment)
         {
             WebHostEnvironment = webHostEnvironment;
         }
 
+        /// <summary>
+        /// Store the information about the web hosting environment an application is running in.
+        /// </summary>
         public IWebHostEnvironment WebHostEnvironment { get; }
 
+        /// <summary>
+        /// The full path of the json file, starting from the web root path.
+        /// </summary>
         private string JsonFileName => Path.Combine(WebHostEnvironment.WebRootPath, "data", "articles.json");
 
+        /// <summary>
+        /// Gets all the data from the json file and creates an instance of the ArticleModel.
+        /// </summary>
+        /// <returns>An Enumerable ArticleModel.</returns>
         public IEnumerable<ArticleModel> GetAllData()
         {
             using var jsonFileReader = File.OpenText(JsonFileName);
@@ -37,8 +55,9 @@ namespace ContosoCrafts.WebSite.Services
         /// If the rating does not exist, add it
         /// Save the update
         /// </summary>
-        /// <param name="articleId"></param>
-        /// <param name="rating"></param>
+        /// <param name="articleId">The unique article Id number.</param>
+        /// <param name="rating">The article rating integer value.</param>
+        /// <returns>Returns true if ratings are added, otherwise return false.</returns>
         public bool AddRating(string articleId, int rating)
         {
             // If the ArticleID is invalid, return
@@ -56,13 +75,13 @@ namespace ContosoCrafts.WebSite.Services
                 return false;
             }
 
-            // Check Rating for boundries, do not allow ratings below 0
+            // Check Rating for boundaries, do not allow ratings below 0
             if (rating < 0)
             {
                 return false;
             }
 
-            // Check Rating for boundries, do not allow ratings above 5
+            // Check Rating for boundaries, do not allow ratings above 5
             if (rating > 5)
             {
                 return false;
@@ -87,7 +106,7 @@ namespace ContosoCrafts.WebSite.Services
         /// Update the fields
         /// Save to the data store
         /// </summary>
-        /// <param name="data"></param>
+        /// <param name="data">The ArticleModel.</param>
         public ArticleModel UpdateData(ArticleModel data)
         {
             var articles = GetAllData();
@@ -115,7 +134,7 @@ namespace ContosoCrafts.WebSite.Services
         }
 
         /// <summary>
-        /// Save All articles data to storage
+        /// Save All articles data to storage.
         /// </summary>
         private void SaveData(IEnumerable<ArticleModel> articles)
         {
@@ -132,10 +151,10 @@ namespace ContosoCrafts.WebSite.Services
         }
 
         /// <summary>
-        /// Create a new article using default values
-        /// After create the user can update to set values
+        /// Create a new article using default values.
+        /// After create the user can update to set values.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>The ArticleModel.</returns>
         public ArticleModel CreateData()
         {
             var data = new ArticleModel()
@@ -147,7 +166,7 @@ namespace ContosoCrafts.WebSite.Services
                 Image = "",
             };
 
-            // Get the current set, and append the new record to it becuase IEnumerable does not have Add
+            // Get the current set, and append the new record to it because IEnumerable does not have Add.
             var dataSet = GetAllData();
             dataSet = dataSet.Append(data);
 
@@ -157,9 +176,9 @@ namespace ContosoCrafts.WebSite.Services
         }
 
         /// <summary>
-        /// Remove the item from the system
+        /// Remove the item from the system.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>The ArticleModel.</returns>
         public ArticleModel DeleteData(string id)
         {
             // Get the current set, and append the new record to it
