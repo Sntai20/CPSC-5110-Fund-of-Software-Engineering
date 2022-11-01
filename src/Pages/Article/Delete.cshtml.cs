a375c83fa5292a91e58076df25ab5b85c09894e1
sharonleo-su <sleo@seattleu.edu>
2022-10-31 23:48:40 -0700

    Create Delete.cshtml.cs to enable deletion of articles.

---
+++ src/Pages/Article/Delete.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+
+namespace ContosoCrafts.WebSite.Pages.Article
+{
+    /// <summary>
+    /// Manage the Delete of the data for a single record
+    /// </summary>
+    public class DeleteModel : PageModel
+    {
+        // Data middletier
+        public JsonFileArticleService ArticleService { get; }
+
+        /// <summary>
+        /// Defualt Construtor
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="articleService"></param>
+        public DeleteModel(JsonFileArticleService articleService)
+        {
+            ArticleService = articleService;
+        }
+
+        // The data to show, bind to it for the post
+        [BindProperty]
+        public ArticleModel Article { get; set; }
+
+        /// <summary>
+        /// REST Get request
+        /// Loads the Data
+        /// </summary>
+        /// <param name="id"></param>
+        public void OnGet(string id)
+        {
+            Article = ArticleService.GetAllData().FirstOrDefault(m => m.Id.Equals(id));
+        }
+
+        /// <summary>
+        /// Post the model back to the page
+        /// The model is in the class variable Product
+        /// Call the data layer to Delete that data
+        /// Then return to the index page
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            ArticleService.DeleteData(Article.Id);
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
