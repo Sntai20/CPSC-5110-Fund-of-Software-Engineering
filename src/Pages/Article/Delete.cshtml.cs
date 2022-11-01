6cbf688066898bf2189413c106de62790025878c
Antonio Santana <ansantan@microsoft.com>
2022-11-01 13:39:07 -0700

    Update the comments in the Delete.cshtml.cs file.

--- src/Pages/Article/Delete.cshtml.cs
+++ src/Pages/Article/Delete.cshtml.cs
@@ -9,24 +9,23 @@ using ContosoCrafts.WebSite.Services;
 namespace ContosoCrafts.WebSite.Pages.Article
 {
     /// <summary>
-    /// Manage the Delete of the data for a single record
+    /// Manage the Delete of the data for a single record.
     /// </summary>
     public class DeleteModel : PageModel
     {
-        // Data middletier
+        // Data middle tier
         public JsonFileArticleService ArticleService { get; }
 
         /// <summary>
-        /// Defualt Construtor
+        /// Default Constructor
         /// </summary>
-        /// <param name="logger"></param>
-        /// <param name="articleService"></param>
+        /// <param name="articleService">The service responsible for interacting with the data store.</param>
         public DeleteModel(JsonFileArticleService articleService)
         {
             ArticleService = articleService;
         }
 
-        // The data to show, bind to it for the post
+        // The data to show, bind to it for the post.
         [BindProperty]
         public ArticleModel Article { get; set; }
 
@@ -34,7 +33,7 @@ namespace ContosoCrafts.WebSite.Pages.Article
         /// REST Get request
         /// Loads the Data
         /// </summary>
-        /// <param name="id"></param>
+        /// <param name="id">The article Id as a string.</param>
         public void OnGet(string id)
         {
             Article = ArticleService.GetAllData().FirstOrDefault(m => m.Id.Equals(id));
@@ -46,7 +45,7 @@ namespace ContosoCrafts.WebSite.Pages.Article
         /// Call the data layer to Delete that data
         /// Then return to the index page
         /// </summary>
-        /// <returns></returns>
+        /// <returns>An IActionResult.</returns>
         public IActionResult OnPost()
         {
             if (!ModelState.IsValid)
