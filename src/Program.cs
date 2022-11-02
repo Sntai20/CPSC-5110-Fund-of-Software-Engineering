6891d81aa761661c198e3ee9366e9ae14ce06ad7
sharonleo-su <sleo@seattleu.edu>
2022-11-01 22:09:29 -0700

    Update Program.cs to include comments.

--- src/Program.cs
+++ src/Program.cs
@@ -3,6 +3,9 @@ namespace ContosoCrafts.WebSite
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.Hosting;
 
+    /// <summary>
+    /// Build and configure the web host.
+    /// </summary>
     public class Program
     {
         public static void Main(string[] args)
@@ -10,6 +13,11 @@ namespace ContosoCrafts.WebSite
             CreateHostBuilder(args).Build().Run();
         }
 
+        /// <summary>
+        /// Create default builder and configure web host defaults.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
