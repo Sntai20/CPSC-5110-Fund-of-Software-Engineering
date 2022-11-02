baea4ca1becb0611c356367b86881808fde3a84a
dheeraja-su <dmalineni@seattleu.edu>
2022-11-01 21:31:50 -0700

    Added required comments in the Privacy.cshtml.cs

--- src/Pages/Privacy.cshtml.cs
+++ src/Pages/Privacy.cshtml.cs
@@ -1,8 +1,11 @@
 ﻿namespace ContosoCrafts.WebSite.Pages
 {
+    using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.RazorPages;
     using Microsoft.Extensions.Logging;
-
+    /// <summary>
+    /// Privacy page
+    /// </summary>
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
@@ -11,7 +14,11 @@
         {
             _logger = logger;
         }
-
+        /// <summary>
+        /// REST Get request
+        /// </summary>
+        
+        
         public void OnGet()
         {
         }
