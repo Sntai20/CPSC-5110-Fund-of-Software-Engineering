c2956758c27ba7c0294f76496779d1a9f36a829b
sharonleo-su <sleo@seattleu.edu>
2022-11-01 22:17:07 -0700

    Update ArticleTypeEnum.cs to include comments.

--- src/Models/ArticleTypeEnum.cs
+++ src/Models/ArticleTypeEnum.cs
@@ -1,5 +1,8 @@
 ﻿namespace ContosoCrafts.WebSite.Models
 {
+    /// <summary>
+    /// Define an enum for type Article. 
+    /// </summary>
     public enum ArticleTypeEnum
     {
         Undefined = 0,
@@ -9,9 +12,16 @@
         Music = 55,
         Food = 63,
     }
-
+    /// <summary>
+    /// Define Article Type enum extensions. 
+    /// </summary>
     public static class ArticleTypeEnumExtensions
     {
+        /// <summary>
+        /// Display name of the Article type enum. 
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
         public static string DisplayName(this ArticleTypeEnum data)
         {
             return data switch
