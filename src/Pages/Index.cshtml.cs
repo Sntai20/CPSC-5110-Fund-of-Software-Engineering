35533585fe27691d16b1b2e2239a13ece9eb59d6
dheeraja-su <dmalineni@seattleu.edu>
2022-11-01 21:45:22 -0700

    Added required comments in the Index.cshtml.cs

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -7,10 +7,15 @@
 
     using Microsoft.AspNetCore.Mvc.RazorPages;
     using Microsoft.Extensions.Logging;
-    
+    /// <summary>
+    /// Index Page will return all the data to show the user.
+    /// </summary>
     public class IndexModel : PageModel
     {
-        
+     /// <summary>
+     /// Default Constructor
+     /// </summary>
+
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger,
@@ -20,11 +25,18 @@
             ArticleService = articleService;
         }
 
-       
+        /// <summary>
+        /// Store the service responsible for interacting with the data store.
+        /// </summary>
         public JsonFileArticleService ArticleService { get; }
-        
+        /// <summary>
+        /// Collection of the Data.
+        /// </summary>
         public IEnumerable<ArticleModel> Articles { get; private set; }
-        
+        /// <summary>
+        /// REST OnGet
+        /// Return all the data
+        /// </summary>
         public void OnGet()
         {
             Articles = ArticleService.GetAllData();
