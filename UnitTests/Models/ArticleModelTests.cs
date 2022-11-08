05f046b80524952fc8bef5834e47b6f9c8059968
AndrewTheLowly <cshell@seattleu.edu>
2022-11-07 21:11:42 -0800

    Added ArticleModelTests.cs to Unit Test folder.

---
+++ UnitTests/Models/ArticleModelTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace UnitTests.Models
+{
+    /// <summary>
+    /// Test class for ArticleModel
+    /// </summary>
+    public class ArticleModelTests
+    {
+        /// <summary>
+        /// Method to test valid input of an article
+        /// </summary>
+        [Test]
+        public void ToString_Valid_Null_ID_Should_Return_True()
+        {
+            // Arrange
+            var data = TestHelper.ArticleService.GetAllData().First(x => x.Id == "jenlooper-cactus");
+
+            // Act
+            var result = TestHelper.ArticleService.GetAllData().First();
+
+            // Assert
+            Assert.AreEqual(data.ToString(), result.ToString());
+
+        }
+    }
+}
