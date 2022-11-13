b792bc867639be3da22909d632439a1cc2c8b0ee
Antonio Santana <ansantan@outlook.com>
2022-11-13 15:34:56 -0800

    Added the About.cshtml.cs file to the Pages folder.

---
+++ src/Pages/About.cshtml.cs
@@ -0,0 +1,33 @@
+﻿namespace ContosoCrafts.WebSite.Pages
+{
+    using System.Collections.Generic;
+    using ContosoCrafts.WebSite.Models;
+    using ContosoCrafts.WebSite.Services;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.Extensions.Logging;
+
+    /// <summary>
+    /// About Page will return team information.
+    /// </summary>
+    public class AboutModel : PageModel
+    {
+        // message logging interface
+        private readonly ILogger<AboutModel> logger;
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="logger">Takes an ILogger.</param>
+        public AboutModel(ILogger<AboutModel> logger)
+        {
+            this.logger = logger;
+        }
+
+        /// <summary>
+        /// REST Get request
+        /// </summary> 
+        public void OnGet()
+        {
+        }
+    }
+}
