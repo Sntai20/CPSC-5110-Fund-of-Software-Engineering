da4ffbda37cc75ed45bb3d22e8e8b7a86e0127c7
Antonio Santana <47947504+Sntai20@users.noreply.github.com>
2022-11-12 19:05:09 -0800

    Fixed formatting in the ErrorModel.cs file.

--- src/Pages/Error.cshtml.cs
+++ src/Pages/Error.cshtml.cs
@@ -1,20 +1,17 @@
 ﻿namespace ContosoCrafts.WebSite.Pages
 {
     using System.Diagnostics;
-
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.RazorPages;
     using Microsoft.Extensions.Logging;
-    /// <summary>
-    /// Error page
-    /// </summary>
-
+    
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
    
-    
+    /// <summary>
+    /// Error page.
+    /// </summary>
     public class ErrorModel : PageModel
     {
-     
         // The data to show, bind to it for the post.
         public string RequestId { get; set; }
 
@@ -26,10 +23,10 @@
         {
             _logger = logger;
         }
+        
         /// <summary>
         /// REST Get request
         /// </summary>
-       
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
