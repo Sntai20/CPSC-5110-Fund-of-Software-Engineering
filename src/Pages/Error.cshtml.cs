93aa71feb97eed39c01c3a11bd2c17ee2bef623f
AndrewTheLowly <cshell@seattleu.edu>
2022-11-19 08:03:50 -0800

    Added comments to properties in Error.cshtml.cs

--- src/Pages/Error.cshtml.cs
+++ src/Pages/Error.cshtml.cs
@@ -15,10 +15,13 @@
         // The data to show, bind to it for the post.
         public string RequestId { get; set; }
 
+        // Boolean check to see if RequestID contains valid value
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
+        // Error logger item
         private readonly ILogger<ErrorModel> _logger;
 
+        // Log an error
         public ErrorModel(ILogger<ErrorModel> logger)
         {
             _logger = logger;
