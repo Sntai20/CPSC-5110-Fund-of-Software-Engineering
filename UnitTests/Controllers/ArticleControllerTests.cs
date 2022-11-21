99bc2cab0773b53d63c4ca2de9f6954f18b67cd7
Antonio Santana <ansantan@microsoft.com>
2022-11-20 16:48:40 -0800

    Added the Get_Valid_Should_Returns_All_Article UT in the ArticleControllerTests.cs file.

--- UnitTests/Controllers/ArticleControllerTests.cs
+++ UnitTests/Controllers/ArticleControllerTests.cs
@@ -1,15 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UnitTests.Controllers
+﻿namespace UnitTests.Controllers
 {
+    using System.Linq;
+    using ContosoCrafts.WebSite.Controllers;
+    using NUnit.Framework;
+
     /// <summary>
-    /// Unit test class for the ArticleController
+    /// Unit tests for the ArticleController class.
     /// </summary>
-    internal class AritcleControllerTests
+    internal class ArticleControllerTests
     {
+        #region TestSetup
+
+        // controller instance to test
+        public static ArticlesController controller;
+
+        /// <summary>
+        /// SetUp for the tests
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            // Create a new controller instance
+            controller = new ArticlesController(TestHelper.ArticleService);
+        }
+
+        #endregion TestSetup
+
+        #region Get
+
+        /// <summary>
+        /// Test the API Get Http request method
+        /// </summary>
+        [Test]
+        public void Get_Valid_Should_Returns_All_Article()
+        {
+            // Arrange
+            var expected = TestHelper.ArticleService.GetAllData().Count();
+
+            // Act
+            var actual = controller.Get().Count();
+
+            // Assert
+            Assert.AreEqual(true, controller.ModelState.IsValid);
+            Assert.AreEqual(expected, actual);
+        }
+
+        #endregion Get
     }
 }
